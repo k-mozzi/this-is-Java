e9190ec697180d828055f070505736628d7379c0
김경모 <kimkm0405@gmail.com>
2022-10-07 18:10:31 +0900

    ch5 exercise

---
+++ this_is_java/ch5/exercise7.java
@@ -0,0 +1,20 @@
+package ch5;
+
+public class exercise7 {
+
+	public static void main(String[] args) {
+
+		int max = 0;
+		int[] array = { 1, 5, 3, 8, 2 };
+
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] > max) {
+				max = array[i];
+			}
+		}
+
+		System.out.println(max);
+
+	}
+
+}
---
+++ this_is_java/ch5/exercise8.java
@@ -0,0 +1,27 @@
+package ch5;
+
+public class exercise8 {
+
+	public static void main(String[] args) {
+
+		int[][] array = { { 95, 86 }, { 83, 92, 96 }, { 78, 83, 93, 87, 88 } };
+
+		int sum = 0;
+		double avg = 0.0;
+		double count = 0.0;
+
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				sum += array[i][j];
+			}
+			count += array[i].length;
+		}
+
+		avg = sum / count;
+
+		System.out.println(sum);
+		System.out.println(avg);
+
+	}
+
+}
